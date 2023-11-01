4bd994235f062b6d3fe57246cd9204ae5232bcaf
NhatHoang <lanhathoang244@gmail.com>
2023-11-01 23:12:13 +0700

    admin product

--- CSShoeWeb-main/src/config/api.js
+++ CSShoeWeb-main/src/config/api.js
@@ -1,7 +1,12 @@
 import { http } from './http'
 
 const API = {
-    login: async (data) => await http.post("/users/authenticate", data)
+    login: async (data) => await http.post("/users/authenticate", data),
+    addProduct: async (data) => await http.post("/shoes/create", data),
+    getBrand: async () => await http.get("/brands"),
+    getListProduct: async () => await http.get("/shoes"),
+    deleteProduct: async (id) => await http.delete(`/shoes/${id}`),
+    getProductById: async (id) => await http.get(`/shoes/${id}`)
 }
 
 export default API
